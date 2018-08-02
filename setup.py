8366c21e66805ca045c9e6fbdce93c7ce2032f74
koh onimaru <koh.onimaru@gmail.com>
2018-08-02 13:48:34 +0900

    fixed instruction in README.rst

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
         'Topic :: Biology/Machine learning',
     
     ],
-    install_requires=['tensorflow-gpu', 'numpy', 'matplotlib'],
+    install_requires=['tensorflow-gpu', 'numpy', 'matplotlib', 'sklearn'],
     long_description=open('README.rst').read(),
 )
 
