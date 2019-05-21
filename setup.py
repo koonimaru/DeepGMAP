2dc7e7abc29feb929c99b6d0ad1fb6ec3c50bc4c
koh <koh@koh-MB-F556>
2019-05-21 09:32:33 +0900

    single version source

--- setup.py
+++ setup.py
@@ -1,7 +1,31 @@
 #from distutils.core import setup
 from setuptools import setup, find_packages
 from distutils.extension import Extension
-VERSION ='0.0'
+import re
+import os
+import codecs
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    # intentionally *not* adding an encoding option to open, See:
+    #   https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]",
+        version_file,
+        re.M,
+    )
+    if version_match:
+        return version_match.group(1)
+
+    raise RuntimeError("Unable to find version string.")
+
 try:
     from Cython.Distutils import build_ext
 except ImportError:
@@ -25,10 +49,11 @@ else:
         Extension("deepgmap.data_preprocessing_tools.seq_to_binary2", [ "deepgmap/data_preprocessing_tools/seq_to_binary2.c" ]),
         Extension("deepgmap.post_train_tools.cython_util", [ "deepgmap/post_train_tools/cython_util.c" ]),
     ]
-
+print(find_version("deepgmap", "__init__.py"))
 setup(
     name='DeepGMAP',
-    version=VERSION,
+    #version=VERSION,
+    version=find_version("deepgmap", "__init__.py"),
     description='Learning and predicting gene regulatory sequences in genomes',
     author='Koh Onimaru',
     author_email='koh.onimaru@gmail.com',
